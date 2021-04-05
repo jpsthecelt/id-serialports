b4ab353a7b97c8f1507f6569c8f9df16b9d694ce
John <jpsthecelt@gmail>
2021-04-05 10:46:56 -0700

    simple, but saved for future use

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace id_serialports {
+    public class Program {
+        public static void Main(string[] args) {
+            String[] PortNames = SerialPort.GetPortNames();
+            
+            Console.WriteLine("Available Ports: [<CR> to exit]");
+            foreach (string p in PortNames) {
+                Console.WriteLine("   {0}", p);
+            }
+            string msg = Console.ReadLine();
+        }
+    }
+}
